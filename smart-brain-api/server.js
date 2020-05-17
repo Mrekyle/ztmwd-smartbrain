c5fcb00ef3f04ee3ca50942e8a8638cf08f18687
Mrekyle <64297454+Mrekyle@users.noreply.github.com>
2020-05-16 20:34:45 -0700

    V4 - Started intergrating Backened Database

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+	client: 'pg',  			// This is tell the knex package where the Database is living and how to log in. With knex, you will also have to install the 
+	connection: {			// Database npm package, Full instructions on the website 
+	    host : '127.0.0.1', // - The same as local host
+	    user : 'kylechart',
+	    password : '',
+	    database : 'smart-brain'
+  }
+});
+
+// db.select('id').from('users').then(data => { 	// This is the basic way of accessing data from a selected table inside the database 
+// 	console.log(data);
+// });
 
 const app = express();
+
+
 
 app.use(express.json()); // To access information given by JSON format, so the server can understand it 
 app.use(express.urlencoded({extended: false})); // Information by post method
@@ -35,55 +52,104 @@ const dataBase = { 			// This is the basic way of creating a test user database 
 }
 
 app.post('/signin', (req, res) => { // This is the basic way of creating a sign in form, checking the database for the user information! And if it matches
-	if (req.body.email === dataBase.users[0].email && 			// Any of the information it sends a success, If not it will throw an error. 
-		req.body.password === dataBase.users[0].password) {
-		res.json(dataBase.users[0]);
-	} else {
-		res.status(400).json('Error Logging In.'); 
-	}
+	db.select('email', 'hash').from('login') 
+	.where('email', '=', req.body.email)	// This is checking if the emails given into the sigin paramater matches the one with the login
+	.then(data => {
+		const isValid = bcrypt.compareSync(req.body.password, data[0].hash);	// This if the email matches, it compares the hash to the password given, as only the server knows the true value
+		if (isValid) {													// And if they match, then it logs in. If not it gives an error
+		return db.select('*').from('users').where('email', '=', req.body.email)
+		.then(user => {
+			res.json(user[0])
+		}) 
+		.catch(err => res.status(400).json('Unable to get user.'))
+		}	
+	})
+	.catch(err => res.status(400).json('Wrong Credentials.'))	
 })
+//		OLD CODE 
+// if (req.body.email === dataBase.users[0].email && 			// Any of the information it sends a success, If not it will throw an error. 
+// 		req.body.password === dataBase.users[0].password) {
+// 		res.json(dataBase.users[0]);
+// 	} else {
+// 		res.status(400).json('Error Logging In.'); 
+// 	}
 
 app.post('/register', (req, res) => {
-	const { email, password, name } = req.body;
-	dataBase.users.push({
-		id: '125',
-		name: name,
-		email: email,
-		entries: 0,
-		joined: new Date(),
-	})	
-	res.json(dataBase.users[dataBase.users.length-1]); 		// This grabs the last user in the array! So we are able to display that informationto the 
-})															// New user
+	const { email, password, name } = req.body;	
+	const hash = bcrypt.hashSync(password);
+	db.transaction(trx => {		// The transaction statmenet here is saying to update the login table first and then update the user table
+		trx.insert({			// But a feature of transactions, means that if one fails they both fail. So you dont get inconsistent database information
+			hash: hash,
+			email: email,
+		})
+		.into('login')
+		.returning('email')		// This returns the email, as the email is the only matching item in each table, adding it to an function allows us to 
+		.then(loginEmail => {	// Store the email, and use the information later on, to update the users table at the same time.
+			return trx('users')
+			.returning('*')			// This db knex code here, is almost identical as before, all it is doing is instead of returing it to the const database
+			.insert({				// it is connecting with the database and adding the information to the table 
+				name: name,
+				email: loginEmail[0], 
+				joined: new Date,
+			})
+			.then(user => { res.json(user[0]);
+		})
+	})
+		.then(trx.commit)		// This is part of the transaction feature, meaning that everytimg you use it, you have to commit the changes to update the tables, Like commiting changes in github
+		.catch(trx.rollback)	// This says if an error happens, it will roll back any changes or updates that have been made to the tables 
+	})
+		.catch(err => res.status(400).json('Unable to Register at this time! Please try again'));
+})	
+
+//		OLD CODE
+	// 	dataBase.users.push({			
+	// 	id: '125',
+	// 	name: name,
+	// 	email: email,
+	// 	entries: 0,
+	// 	joined: new Date(),
+	// })														
+// res.json(dataBase.users[dataBase.users.length-1]); 	This grabs the last user in the array! So we are able to display that informationto the New user
 
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;				// This allows us to get the id paramater of the user that has logged into the system
-	let found = false;						// This is saying, that if the user doesnt exist until the user has been found and turned to true, in which case it will return the user information
-	dataBase.users.forEach(user => {		// This is allowing us to loop through the database to get the correct corisponding information
-		if (user.id === id) {				// This is saying that if it finds a match, it will respond with the user information
-			found = true;					 
-			return res.json(user);
-		} 									
-	})
-	if (!found) {
-		res.status(404).json('No Such User');	// This is saying that if it cant find a match for the user, then there is no such user in the database
-	}
-
+	db.select('*').from('users').where({id})				// This is asking the database for the user information associated to the id! And asking it
+	.then(user => { if (user.length) { res.json(user[0])	// To return the information, or if the user id doesnt exist, let us know
+	} else { res.status(400).json('User does not exist.')}
+		})
+	.catch(err => res.status(400).json('Error finding user.'))
 })
+
+// 		OLD CODE
+// let found = false; 			 This is saying, that if the user doesnt exist until the user has been found and turned to true, in which case it will return the user information
+// dataBase.users.forEach(user => {		This is allowing us to loop through the database to get the correct corisponding information
+// 	if (user.id === id) {				 This is saying that if it finds a match, it will respond with the user information
+// 		found = true;					 
+// 		return res.json(user);
+// 	} 									
+// }) if (!found) {
+		// res.status(404).json('No Such User'); })	This is saying that if it cant find a match for the user, then there is no such user in the database
 
 app.put('/image', (req, res) => {  				// This whole section of code here is finding the user like above, and matching it wiith the database.
-	const { id } = req.body;					// Then it is finding the entries and adding 1 to the total every time that user inputs an image
-	let found = false;							// inot the app 
-	dataBase.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			user.entries++
-			return res.json(user.entries);
-		}
-	})
-	if (!found) {
-		res.status(404).json('No Such User');
-	}
-})
+	const { id } = req.body;					// Then it is finding the entries and adding 1 to the total every time that user inputs an image into the app 
+	db('users').where('id', '=', id)			// This is incrementing the entries in the database by 1 each time it is called
+	.increment('entries', 1).returning('entries').then(entries => { res.json(entries[0])
+	})	
+	.catch(err => res.status(400).json('Error updating entries!')) 	// Standard error code/syntax 
+})	
+
+//		OLD CODE
+// let found = false;							
+// 	dataBase.users.forEach(user => {
+// 		if (user.id === id) {
+// 			found = true;
+// 			user.entries++
+// 			return res.json(user.entries);
+// 		}
+// 	})
+// 	if (!found) {
+// 		res.status(404).json('No Such User');
+// 	}
 
 app.listen(3000, () => {	// Always have this as the final code on the page!
 	console.log('Loud and clear Homie!'); // This is the basic way of knowing weather your server is actually working and responding to any requests that are made to it! 
@@ -105,5 +171,4 @@ to make it easier to read and run more efficently!
 /register --> POST = Add new users to the database
 /profile/:id --> GET = Will return user information
 /image --> PUT = Update user score based on how many images they have sumbitte to the applicaiton
-
 */
